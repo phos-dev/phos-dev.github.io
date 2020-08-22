32a1ae0c33f2ac8467a1968d5defe751eb77cc5f
Pedro Henrique <38169759+phos-dev@users.noreply.github.com>
2020-08-22 11:02:18 -0300

    Updates

---
+++ precache-manifest.7c85fa5f0f5c2eecab1e14692c1bae1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a2c065e3f52d5163203fd9ba62314e",
+    "url": "/recogF/index.html"
+  },
+  {
+    "revision": "3a96d7eb91ba2c90aad9",
+    "url": "/recogF/static/css/main.9d60369b.chunk.css"
+  },
+  {
+    "revision": "131e2572cd2d5a0c1c3b",
+    "url": "/recogF/static/js/2.c993fb7e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/recogF/static/js/2.c993fb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a96d7eb91ba2c90aad9",
+    "url": "/recogF/static/js/main.43a17a00.chunk.js"
+  },
+  {
+    "revision": "e798105e4b4e0bdafe71",
+    "url": "/recogF/static/js/runtime-main.e13a9b53.js"
+  },
+  {
+    "revision": "949d449aedf492e76f6631666bded7ad",
+    "url": "/recogF/static/media/logo.949d449a.png"
+  }
+]);
